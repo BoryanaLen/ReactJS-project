ee899ebb1f3a9012b4bc0a8086dc6fe045f69e60
BoryanaLen <boryana.lengerova@semansys.com>
2022-08-09 00:29:21 +0300

    role based control

--- smart-hr-system/client/src/components/jobs/JobsList.jsx
+++ smart-hr-system/client/src/components/jobs/JobsList.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { headerlogo } from '../../assets/imagepath'
-import { JobDetails } from './JobDetails'
 import * as jobsService from '../../services/jobsService'
 
 export const JobsList = () => {
