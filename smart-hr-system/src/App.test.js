fd1b207329281b1cfb730466b85563d5b72a4265
BoryanaLen <boryana.lengerova@semansys.com>
2022-08-10 22:35:10 +0300

    fix App.test.js

--- smart-hr-system/src/App.test.js
+++ smart-hr-system/src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import {BrowserRouter} from 'react-router-dom'
 import App from './App';
+import { AuthContext } from './contexts/AuthContext';
+
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+    <AuthContext.Provider value={{ userLogin: jest.fn(), role: 'user' }}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>   
+    </AuthContext.Provider>);
+  const linkElement = screen.getByText('Access to our dashboard');
   expect(linkElement).toBeInTheDocument();
 });
